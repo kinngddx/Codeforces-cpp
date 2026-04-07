61a20fad70f38c1fc35c16efe14638f4033e4abb
kinngddx <ddxboss70@gmail.com>
2026-04-07 22:01:27 +0530

    Arrays

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    long long ans = LLONG_MAX;
+    for(int i =0;i<n;i++){
+        long long x;
+        cin>>x;
+        ans = min (ans,abs(x));
+    }
+
+    cout<<ans<<endl;
+    
+    return 0;
+}
