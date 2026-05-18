0888bee17d501b318f1610abcf33a1ce59051ff9
kinngddx <ddxboss70@gmail.com>
2026-05-18 20:14:05 +0530

    @ solved

---
+++ B_Digit_String.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int n = s.size();
+        vector<int> suf(n + 1, 0);
+
+
+        
+        for (int i = n - 1; i >= 0; --i) {
+            suf[i] = suf[i + 1] + (s[i] == '1' || s[i] == '3');
+        }
+
+
+
+        int ans = 0, pref2 = 0;
+
+
+
+        for (int i = 0; i <= n; ++i) {
+            ans = max(ans, pref2 + suf[i]);
+            if (i < n && s[i] == '2') pref2++;
+        }
+
+
+
+
+
+
+
+
+        cout << n - ans << '\n';
+    }
+
+    return 0;
+}
